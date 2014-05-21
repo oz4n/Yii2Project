52dd18d5bbfa6096557b79e759fca106bbe97e1a
oz4n <oz4n.rock@gmail.com>
2014-05-22 00:44:20 +0800

    add modules file

---
+++ config/modules.php
@@ -0,0 +1,36 @@
+<?php
+return [
+    'gii' => [
+        'class' => 'yii\gii\Module',
+    ],
+    'dashboard' => [
+        'class' => 'app\modules\dashboard\dashboard',
+    ],
+    'word' => [
+        'class' => 'app\modules\word\word',
+    ],
+    'member' => [
+        'class' => 'app\modules\member\Member',
+    ],
+    'userlog' => [
+        'class' => 'app\modules\userlog\UserLog',
+        'log_tatus' => false
+    ],
+    'filemanager' => [
+        'class' => 'app\modules\filemanager\FileManager',
+    ],
+    'guestbook' => [
+        'class' => 'app\modules\guestbook\GuestBook'
+    ],
+    'user' => [
+        'class' => 'dektrium\user\Module',
+        'allowUnconfirmedLogin' => true,
+        'confirmWithin' => 21600,
+        'cost' => 12,
+        'admins' => ['ozan']
+    ],
+    'site' => [
+        'class' => 'app\modules\site\site',
+    ],
+
+];
