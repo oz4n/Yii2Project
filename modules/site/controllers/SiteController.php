88c17b839e3650ff0bdaf9c0be18aa17b31aa290
oz4n <oz4n.rock@gmail.com>
2014-05-31 03:41:53 +0800

    image media and menu pront end

--- modules/site/controllers/SiteController.php
+++ modules/site/controllers/SiteController.php
@@ -2,7 +2,9 @@
 
 namespace app\modules\site\controllers;
 
+use Yii;
 use yii\web\Controller;
+use app\modules\site\searchs\PostSerch;
 
 class SiteController extends Controller
 {
@@ -20,75 +22,110 @@ class SiteController extends Controller
         ];
     }
 
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
-
-    }
-
     public function actionIndex()
     {
         return $this->render('index');
     }
 
-    public function actionInfo()
+    public function actionTax()
     {
-        return $this->render('info');
+        $model = new PostSerch;
+        $dataProvider = $model->search(Yii::$app->request->getQueryParams());
+        $param = Yii::$app->request->getQueryParams();
+        return $this->render('tax', [
+                    'dataProvider' => $dataProvider,
+                    'searchModel' => $model,
+                    'tax' => $model->findTaxNameBySLug($param['tax']),
+        ]);
     }
 
-    public function actionInfo1()
+    public function actionView()
     {
-        return $this->render('info_1');
+        
+        $model = new PostSerch;
+        return $this->render('view', [
+            'model' => $this->getPostSlug($model),
+            'tax' => $this->getTaxName($model),
+        ]);
+    }
+    
+    protected function getPostSlug($model)
+    {
+        $param = Yii::$app->request->getQueryParams();
+        if (isset($param['slug'])) {
+            return $model->findPostBySlug($param['slug']);
+        } else {
+            throw new \yii\web\NotFoundHttpException('The requested page does not exist.');
+        }
     }
 
-    public function actionInfo2()
+    protected function getTaxName($model)
     {
-        return $this->render('info_2');
+        $param = Yii::$app->request->getQueryParams();
+        if (isset($param['tax'])) {
+            return $model->findTaxNameBySLug($param['tax']);
+        } else {
+            throw new \yii\web\NotFoundHttpException('The requested page does not exist.');
+        }
     }
 
-    public function actionInfo3()
-    {
-        return $this->render('info_2');
-    }
-
-    public function actionInfo4()
-    {
-        return $this->render('info_2');
-    }
-
-    public function actionInfo5()
-    {
-        return $this->render('info_5');
-    }
-
-    public function actionInfo6()
-    {
-        return $this->render('info_2');
-    }
-
-    public function actionAbout()
-    {
-        return $this->render('about');
-    }
-
-    public function actionAbout1()
-    {
-        return $this->render('about_1');
-    }
-
-    public function actionContact()
-    {
-        return $this->render('contact');
-    }
-
-    public function actionLogbook()
-    {
-        return $this->render('logbook');
-    }
-
-    public function actionRegulation()
-    {
-        return $this->render('regulation');
-    }
-
+//    public function actionInfo()
+//    {
+//        return $this->render('info');
+//    }
+//
+//    public function actionInfo1()
+//    {
+//        return $this->render('info_1');
+//    }
+//
+//    public function actionInfo2()
+//    {
+//        return $this->render('info_2');
+//    }
+//
+//    public function actionInfo3()
+//    {
+//        return $this->render('info_2');
+//    }
+//
+//    public function actionInfo4()
+//    {
+//        return $this->render('info_2');
+//    }
+//
+//    public function actionInfo5()
+//    {
+//        return $this->render('info_5');
+//    }
+//
+//    public function actionInfo6()
+//    {
+//        return $this->render('info_2');
+//    }
+//
+//    public function actionAbout()
+//    {
+//        return $this->render('about');
+//    }
+//
+//    public function actionAbout1()
+//    {
+//        return $this->render('about_1');
+//    }
+//
+//    public function actionContact()
+//    {
+//        return $this->render('contact');
+//    }
+//
+//    public function actionLogbook()
+//    {
+//        return $this->render('logbook');
+//    }
+//
+//    public function actionRegulation()
+//    {
+//        return $this->render('regulation');
+//    }
 }
