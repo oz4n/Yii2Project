ceb21258748a6c35a998827967c104cd46fa5a6f
oz4n <oz4n.rock@gmail.com>
2014-06-07 19:23:53 +0800

    Add user RBAC module

---
+++ modules/userrbac/UserRbac.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\userrbac;
+
+class UserRbac extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\userrbac\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
