8c16611267a388e888578d21e5322b60a96853d4
oz4n <oz4n.rock@gmail.com>
2014-06-13 13:43:24 +0800

    module console

---
+++ modules/console/Console.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\console;
+
+class Console extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\console\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
